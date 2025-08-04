7d6234ad8991c7d181de9fe3a00093c23628b26b
KISHORE <kishore@KISHOREs-MacBook-Air.local>
2025-08-05 00:16:26 +0530

    cors orgin modified

--- model/main.py
+++ model/main.py
@@ -13,7 +13,7 @@ app=FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://heart-disease-predictor-phi.vercel.app/"],
+    allow_origins=["https://heart-disease-predictor-phi.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
